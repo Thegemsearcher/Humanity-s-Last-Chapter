2d99c5fdd532e59f391a9a8a15c946e00574dcab
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-09-18 19:03:00 +0200

    Scene Switcheroo

---
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GoToHub() {
+        SceneManager.LoadScene("Main");
+    }
+    public void GoToMission() {
+        SceneManager.LoadScene("MissionMap");
+    }
+}
