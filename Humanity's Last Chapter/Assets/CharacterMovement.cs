7f6dd189f844f90bde61f33ac09d2aee93bc0646
ChaacNado <35525138+ChaacNado@users.noreply.github.com>
2019-09-22 18:30:50 +0200

    Character Movement 0.1
    La till en karaktär i scenen och påbörjat med basic waypoint movement

---
+++ Humanity's Last Chapter/Assets/CharacterMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+    }
+}
