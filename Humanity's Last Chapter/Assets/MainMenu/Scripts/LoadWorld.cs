c180fb2a34dc1d6a4aec5d460d3bcf0062dd035f
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-09-23 17:19:28 +0200

    Skapar Character och Party save folder om den inte finns

---
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/LoadWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class LoadWorld : MonoBehaviour {
+    GameObject characterO;
+    public GameObject character;
+    int characterCounter;
+
+    void Start() {
+        Debug.Log("LW: Start");
+        if(!Directory.Exists(Application.persistentDataPath + "/Characters")) {
+            Debug.Log("It got here");
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Application.persistentDataPath; //vi behöver något sätt att se till att de inte sparar över varandra.. tänkte använda id men det blir ju också raderat
+            Directory.CreateDirectory(path + "/Party");
+            Directory.CreateDirectory(path + "/Characters");
+        }
+        else {
+            characterCounter = Directory.GetFiles(Application.persistentDataPath + "/Characters/").Length;
+            for (int i = 0; i < characterCounter; i++) { //Gör vi såhär så raderar vi inte character utan kanske har en bool som säger om de ska visas eller inte
+                characterO = Instantiate(character, new Vector2(0, 0), Quaternion.identity) as GameObject;
+                characterO.transform.parent = GameObject.Find("CharacterManager").transform;
+                characterO.GetComponent<CharacterScript>().LoadPlayer(i);
+            }
+        }
+    }
+    
+}
--- Humanity's Last Chapter/Assets/MainMenu/Scripts/Recruit.cs
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/Recruit.cs
@@ -8,30 +8,11 @@ public class Recruit : MonoBehaviour {
     public GameObject character;
     CharacterScript characterS;
     int characterCounter;
-
-    //string info = new DirectoryInfo(Application.persistentDataPath + "/characters/");
     
-
-    void Start() { //Planen här är att den ska hitta alla filer med info i sig o skapa en karaktär av den
-        //Tror det behövs instantiate en karaktär men vet inte hur många som kommer behövas
-
-        characterCounter = System.IO.Directory.GetFiles(Application.persistentDataPath + "/Characters/").Length;
-        for (int i = 0; i < characterCounter; i++) { //Gör vi såhär så raderar vi inte character utan kanske har en bool som säger om de ska visas eller inte
-            characterO = Instantiate(character, new Vector2(0, 0), Quaternion.identity) as GameObject;
-            characterO.transform.parent = GameObject.Find("CharacterManager").transform;
-            characterO.GetComponent<CharacterScript>().LoadPlayer(i);
-        }
-        //foreach (string file in System.IO.Directory.GetFiles(Application.persistentDataPath + "/characters/")) {
-        //    characterO = Instantiate(character, new Vector2(-5, -5), Quaternion.identity) as GameObject;
-        //    characterO.transform.parent = GameObject.Find("CharacterManager").transform;
-        //    characterO.
-        //}
-    }
-
     public void RecruitCharacter() {//OBS! med en positionen kommer dom aldrig hamna i närheten av skärmen
                                     //OBSOBS! om du försöker använda det här för att skapa karaktärerna i huben när dom kommer tillbaks från ett mission 
                                             //så kan du istället använda CampScript.CreateCharacter (den kanske måste modifieras lite men det är lättare                         så )
-        characterO = Instantiate(character, new Vector2(-5, -5), Quaternion.identity) as GameObject;
+        characterO = Instantiate(character, new Vector2(0, 0), Quaternion.identity) as GameObject;
         characterO.transform.parent = GameObject.Find("CharacterManager").transform; //Lägger in objectet i CharacterManager för att lättar ha koll på de
     }
 }
