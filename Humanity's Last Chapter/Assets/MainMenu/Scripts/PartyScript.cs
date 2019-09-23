7bd49b83ac7832727a6cba143429b4fd53921b2b
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-09-23 12:43:11 +0200

    Fixade fellet i PartyScript och Recruit

--- Humanity's Last Chapter/Assets/MainMenu/Scripts/PartyScript.cs
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/PartyScript.cs
@@ -8,7 +8,7 @@ public class PartyScript : MonoBehaviour {
     int characterCounter;
 
     void Start() {
-        characterCounter = System.IO.Directory.GetFiles(Application.persistentDataPath + "/characters/").Length;
+        characterCounter = System.IO.Directory.GetFiles(Application.persistentDataPath + "/Characters/").Length;
         for (int i = 0; i < characterCounter; i++) { //Gör vi såhär så raderar vi inte character utan kanske har en bool som säger om de ska visas eller inte
             characterO = Instantiate(character, new Vector2(0, 0), Quaternion.identity) as GameObject;
             characterO.transform.parent = GameObject.Find("PCManager").transform;
--- Humanity's Last Chapter/Assets/MainMenu/Scripts/Recruit.cs
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/Recruit.cs
@@ -15,7 +15,7 @@ public class Recruit : MonoBehaviour {
     void Start() { //Planen här är att den ska hitta alla filer med info i sig o skapa en karaktär av den
         //Tror det behövs instantiate en karaktär men vet inte hur många som kommer behövas
 
-        characterCounter = System.IO.Directory.GetFiles(Application.persistentDataPath + "/characters/").Length;
+        characterCounter = System.IO.Directory.GetFiles(Application.persistentDataPath + "/Characters/").Length;
         for (int i = 0; i < characterCounter; i++) { //Gör vi såhär så raderar vi inte character utan kanske har en bool som säger om de ska visas eller inte
             characterO = Instantiate(character, new Vector2(0, 0), Quaternion.identity) as GameObject;
             characterO.transform.parent = GameObject.Find("CharacterManager").transform;
