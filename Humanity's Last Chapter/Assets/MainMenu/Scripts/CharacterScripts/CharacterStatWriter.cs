60b6784a9a5746181142194b682a5e781c269664
patrik-nilsson <patriknilson@hotmail.com>
2019-09-26 19:11:03 +0200

    Added stats on canvas
    Added stats visible on characters' canvas.
    Created a new script: CharacterStatWriter. The script is a component of the Prefab CharacterCanvas.
    Stats.cs has been edited to run the method in CharacterStatWriter.cs.

---
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/CharacterScripts/CharacterStatWriter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStatWriter : MonoBehaviour
+{
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetStats(int hp, int str, int def)
+    {
+        text.GetComponent<Text>().text = "Health: " + hp + "\nStrength: " + str + "\nDefence: " + def;
+    }
+}
--- Humanity's Last Chapter/Assets/MainMenu/Scripts/CharacterScripts/stats.cs
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/CharacterScripts/stats.cs
@@ -8,17 +8,22 @@ public class stats : MonoBehaviour
     public int hp, str, def;
     public GameObject characterUI;
     public GameObject prefabCharacterUI;
+    public CharacterStatWriter writer;
     // Start is called before the first frame update
     void Start()
     {
+        hp = Random.Range(1, 11); //Just for show.
         characterUI = Instantiate(prefabCharacterUI, new Vector3(0,0,0), Quaternion.identity);
         characterUI.GetComponentInChildren<Button>().onClick.AddListener(delegate { characterUI.SetActive(false); });
+        //Set the script to the instance of the CharacterCanvas object, and then run the method in it.
+        writer = prefabCharacterUI.GetComponent<CharacterStatWriter>();
+        writer.GetStats(hp, str, def);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void BringUpStats()
     {
