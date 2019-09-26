f8cf9ca4388deb36f00c379cd4bd5c1405015007
David <davidskiold@yahoo.se>
2019-09-26 14:07:30 +0200

    påbörjat ui för formationer

---
+++ Humanity's Last Chapter/Assets/FormationsScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationsScript : MonoBehaviour
+{
+    public GameObject panelForCharacters;
+    GameObject[] pcs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenFormation()
+    {
+        pcs = GameObject.FindGameObjectsWithTag("Character");
+        for (int i = 0; i < pcs.Length; i++)
+        {
+
+        }
+    }
+}
