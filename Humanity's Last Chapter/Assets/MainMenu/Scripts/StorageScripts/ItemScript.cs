500ac4d7fbb29d8b24c02caf1b655945bc5f61bd
patrik-nilsson <patriknilson@hotmail.com>
2019-09-27 16:05:41 +0200

    Added Storage
    There's now a storage and item prefab with scripts.

---
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/StorageScripts/ItemScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    bool held = false;
+    Vector3 slotPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !held)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (GetComponent<Collider2D>().OverlapPoint(mousePosition))
+            {
+                if (!held)
+                {
+                    held = true;
+                }
+            }
+        }
+        else if (Input.GetMouseButton(0) && held)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            gameObject.GetComponent<Transform>().position = mousePosition;
+        }
+        else if (held)
+        {
+            held = false;
+            gameObject.GetComponent<Transform>().position = slotPosition;
+        }
+    }
+
+    public void SetSlot(Vector3 SlotPosition)
+    {
+        slotPosition = SlotPosition;
+    }
+
+    public GameObject CreateItem()
+    {
+        return gameObject;
+    }
+}
---
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/StorageScripts/StorageLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StorageLoader : MonoBehaviour
+{
+    public GameObject StoragePrefab;
+    public GameObject ItemPrefab;
+    List<GameObject> items = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<Button>().onClick.AddListener(delegate { StoragePrefab.SetActive(false); });
+        Instantiate(ItemPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        items.Add(ItemPrefab.GetComponent<ItemScript>().CreateItem());
+        int i = 0;
+        foreach(GameObject item in items)
+        {
+            item.GetComponent<ItemScript>().SetSlot(new Vector3(GetComponent<RectTransform>().position.x + i * 15, GetComponent<RectTransform>().position.y, 0));
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddItem(GameObject go)
+    {
+        
+    }
+}
