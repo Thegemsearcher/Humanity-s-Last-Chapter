122b175db8ac554a8754f8fe6eccee49bba7002f
David <davidskiold@yahoo.se>
2019-10-03 15:44:40 +0200

    mergeförsök

--- Humanity's Last Chapter/Assets/HireCharacter.cs
+++ Humanity's Last Chapter/Assets/HireCharacter.cs
@@ -1,28 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HireCharacter : MonoBehaviour {
     public GameObject HireBTN;
-    private GameObject Manager;
+    public Text txtHire;
+    private GameObject Manager, World;
+    public int cost;
     private int childCounter;
     private CharacterScript characterScript;
     private UIBoiScript boiScript;
+    private stats statsScript;
 
     private void Start() {
         Manager = GameObject.FindGameObjectWithTag("CharacterManager");
+        World = GameObject.Find("WorldManager");
+        statsScript = GetComponent<stats>();
+
+        cost = statsScript.GetCost();
+        txtHire.text = "Hire! (" + cost + ")";
     }
 
     public void btnHire() {
         characterScript = GetComponent<CharacterScript>();
         boiScript = GetComponent<UIBoiScript>();
+        
+        if(cost <= World.GetComponent<WorldScript>().gold) {
+            World.GetComponent<WorldScript>().gold -= cost;
+            childCounter = Manager.transform.childCount;
+            transform.SetParent(Manager.transform, false);
+            characterScript.GetID();
+            boiScript.GetPos(childCounter);
+            GetComponent<UIBoiScript>().isOwned = true;
+            HireBTN.SetActive(false);
+        }
+        else {
+            Debug.Log("Not enough gold!");
+        }
 
-        childCounter = Manager.transform.childCount;
-        transform.SetParent(Manager.transform, false);
-        characterScript.GetID();
-        boiScript.GetPos(childCounter);
-        GetComponent<UIBoiScript>().isOwned = true;
-        HireBTN.SetActive(false);
+        
     }
 
     public void ShowBTN() {
--- Humanity's Last Chapter/Assets/MainMenu/Scripts/BarrackWindow.cs
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/BarrackWindow.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class BarrackWindow : MonoBehaviour {
 
+    private GameObject[] buildings;
+
+    private void Start() {
+        buildings = GameObject.FindGameObjectsWithTag("Building");
+    }
+
     public void btnExit() {
+        //foreach (GameObject building in buildings) {
+        //    building.SetActive(true);
+        //}
         gameObject.SetActive(false);
     }
 }
