5fad79ff95c94bac1011fe840cdc71e9208cb01b
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-10-04 09:58:31 +0200

    Lite små fixes i Backyard

--- Humanity's Last Chapter/Assets/Spawner.cs
+++ Humanity's Last Chapter/Assets/Spawner.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Spawner : MonoBehaviour {
-    public GameObject Enemy, Neutral, QuestGiver;
-    private GameObject parent;
+    public GameObject Enemy, Neutral, QuestGiver, character;
+    private GameObject parent, characterO;
     public Tilemap tilemap;
     private Vector3 place;
     private Vector3Int localPlace;
-    private List<Vector3> enemySpawn, questGiverSpawn, neutralSpawn;
+    private List<Vector3> enemySpawn, questGiverSpawn, neutralSpawn, characterSpawn;
+    private int[] missionOrder;
+    private int characterCounter;
 
     // Start is called before the first frame update
     void Start() {
         enemySpawn = new List<Vector3>();
         questGiverSpawn = new List<Vector3>();
         neutralSpawn = new List<Vector3>();
+        characterSpawn = new List<Vector3>();
+        missionOrder = SaveSystem.LoadPartyOrder();
+        characterCounter = 0;
 
         for (int i = tilemap.cellBounds.xMin; i < tilemap.cellBounds.xMax; i++) {
             for (int j = tilemap.cellBounds.yMin; j < tilemap.cellBounds.yMax; j++) {
                 localPlace = (new Vector3Int(i, j, (int)tilemap.transform.position.y));
                 place = tilemap.CellToWorld(localPlace);
                 if(tilemap.HasTile(localPlace)) {
-                    Debug.Log("GetSprite: " + tilemap.GetSprite(localPlace).ToString());
                     switch(tilemap.GetSprite(localPlace).ToString()) {
                         case "BadBoi (UnityEngine.Sprite)":
                             enemySpawn.Add(place);
@@ -32,6 +36,12 @@ public class Spawner : MonoBehaviour {
                             break;
                         case "QuestGiver (UnityEngine.Sprite)":
                             questGiverSpawn.Add(place);
+                            break;
+                        case "HumanityBoi (UnityEngine.Sprite)":
+                            if(characterCounter < missionOrder.Length) {
+                                characterSpawn.Add(place);
+                                characterCounter++;
+                            }
                             break;
                     }
                 }
@@ -49,6 +59,21 @@ public class Spawner : MonoBehaviour {
         foreach (Vector3 place in questGiverSpawn) {
             parent = GameObject.Find("QuestGiverManager");
             CreateTarget(place, QuestGiver, parent.transform);
+        }
+        characterCounter = 0;
+        foreach (Vector3 place in characterSpawn) {
+            
+            if(missionOrder[characterCounter] >= 0) {
+                Debug.Log("Does it get here?");
+                characterO = Instantiate(character, place, Quaternion.identity) as GameObject;
+                characterO.transform.parent = GameObject.FindGameObjectWithTag("CharacterManager").transform;
+                characterO.transform.localScale = new Vector3(1, 1, 1);
+                characterO.GetComponent<CharacterScript>().LoadPlayer(missionOrder[characterCounter]);
+                characterO.GetComponent<PersonalMovement>().relativePos = new Vector3(characterCounter * 0.5f, characterCounter * 0.5f);
+                characterO.GetComponent<PersonalMovement>().AddRelativeWaypoint(characterO.transform.parent.position);
+                characterCounter++;
+            }
+            
         }
         tilemap.ClearAllTiles();
     }
