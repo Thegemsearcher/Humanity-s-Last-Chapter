565d8082f470c09fccdd8a0aa77da7d5e20b5493
David <davidskiold@yahoo.se>
2019-10-08 17:33:25 +0200

    david och jack lägger till saker i fiendernas bt och krashar programmet tjohoo

--- Humanity's Last Chapter/Assets/WanderingEnemy.cs
+++ Humanity's Last Chapter/Assets/WanderingEnemy.cs
@@ -2,36 +2,86 @@
 using System.Collections.Generic;
 using Pathfinding;
 using UnityEngine;
+using static Node;
 
 public class WanderingEnemy : MonoBehaviour
 {
-    IAstarAI enemyAI;
-    GameObject[] waypoints;
+    IAstarAI enemyAstar;
+    Vector3[] waypoints;
     int currentWP;
+    float offset = 1.5f;
+    RootNode BT;
     // Start is called before the first frame update
     void Start()
     {
-        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
-        enemyAI = GetComponent<IAstarAI>();
+        waypoints = new Vector3[3];
+        //waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        Vector3 toAdd = new Vector3(Random.Range(transform.position.x - offset, transform.position.x + offset), 
+            Random.Range(transform.position.y - offset, transform.position.y + offset), 0);
+        waypoints[0] = toAdd;
+        toAdd = new Vector3(Random.Range(transform.position.x - offset, transform.position.x + offset),
+            Random.Range(transform.position.y - offset, transform.position.y + offset), 0);
+        waypoints[1] = toAdd;
+        toAdd = new Vector3(Random.Range(transform.position.x - offset, transform.position.x + offset),
+            Random.Range(transform.position.y - offset, transform.position.y + offset), 0);
+        waypoints[2] = toAdd;
+
+        enemyAstar = GetComponent<IAstarAI>();
         currentWP = 0;
-        enemyAI.destination = waypoints[currentWP].transform.position;
+        //enemyAstar.destination = waypoints[currentWP];
+
+        BT = GetComponent<BehvaiourTree>().GetEnemyBt();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (waypoints.Length == 0)
+        if(BT == null)
         {
+            Debug.Log("inget BT");
             return;
         }
-        if (Vector2.Distance(waypoints[currentWP].transform.position, enemyAI.position ) < 3.0f)
+
+        BT.Start();
+        //if (waypoints.Length == 0)
+        //{
+        //    return;
+        //}
+        //if (Vector2.Distance(waypoints[currentWP], enemyAstar.position ) < 3.0f)
+        //{
+        //    currentWP++;
+        //    enemyAstar.destination = waypoints[currentWP];
+        //    if (currentWP >= waypoints.Length)
+        //    {
+        //        currentWP = 0;
+        //    }
+        //}
+    }
+
+    public NodeStates CloseToWaypoint()
+    {
+        if (waypoints.Length == 0)
         {
-            currentWP++;
-            enemyAI.destination = waypoints[currentWP].transform.position;
-            if (currentWP >= waypoints.Length)
-            {
-                currentWP = 0;
-            }
+            return NodeStates.fail;
         }
+        //Debug.Log("" + currentWP);
+        if (Vector2.Distance(waypoints[currentWP], enemyAstar.position) < 3.0f)
+        {
+            return NodeStates.success;
+        }
+
+        return NodeStates.fail;
+    }
+
+    public NodeStates NextWaypoint()
+    {
+        currentWP++;
+        if (currentWP < waypoints.Length)
+            enemyAstar.destination = waypoints[currentWP];
+        if (currentWP > waypoints.Length)
+        {
+            currentWP = 0;
+        }
+        return NodeStates.success;
     }
 }
