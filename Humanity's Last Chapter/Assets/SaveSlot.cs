a1ca15e08ff0cdca16b255b4fde7ecc3287f26ea
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-11-11 01:21:52 +0100

    Påbörjad nytt utseende för Load delen

---
+++ Humanity's Last Chapter/Assets/SaveSlot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSlot : MonoBehaviour {
+
+    public int activeSave;
+
+    public void ActivateSave() {
+        //WorldScript.
+    }
+}
