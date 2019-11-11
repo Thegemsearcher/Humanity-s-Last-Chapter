29c51f70262c16a8da8f317d609b166b09ed1334
David <davidskiold@yahoo.se>
2019-11-12 00:45:13 +0100

    hot mess

--- Humanity's Last Chapter/Assets/MainMenu/Scripts/BuildingScripts/HubCharController.cs
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/BuildingScripts/HubCharController.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class HubCharController : MonoBehaviour
 {
-
-
     private List<GameObject> playerRoster = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
@@ -30,7 +28,6 @@ public class HubCharController : MonoBehaviour
             i++;                                  //Removed the off-sets here, because I think they should only be in the CampScript (maybe not?) YES IT IS NEEDED
             character.GetComponent<RectTransform>().position = new Vector3(character.transform.parent.position.x - 0.6f,
                                                                             (character.transform.parent.position.y + 3.3f) - (i * 1), 0);
-            
         }
     }
 
--- Humanity's Last Chapter/Assets/UIControls.cs
+++ Humanity's Last Chapter/Assets/UIControls.cs
@@ -11,6 +11,10 @@ public class UIControls : MonoBehaviour
     Vector3 namePlateOffset = new Vector3(-0.255f, 0.45f,0);
     public GameObject namePlate;
     GameObject currentNamePlate;
+
+    GameObject ParentForUiRepresentation;
+    public GameObject UiRepresentation;
+    GameObject currentUiRepresentation;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +24,11 @@ public class UIControls : MonoBehaviour
         currentNamePlate = Instantiate(namePlate);
         currentNamePlate.GetComponent<TextMesh>().text = GetComponent<CharacterScript>().name;
         currentNamePlate.GetComponent<MeshRenderer>().sortingOrder = 1;
+
+        //ParentForUiRepresentation = GameObject.Find("CharacterHolder");
+        //currentUiRepresentation = Instantiate(UiRepresentation);
+        //currentUiRepresentation.transform.SetParent(ParentForUiRepresentation.transform);
+        //currentUiRepresentation.transform.localPosition = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
