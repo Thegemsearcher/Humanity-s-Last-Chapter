b2b17ad722945ee5c8ebd48b3a41127034b6acba
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-11-16 16:20:53 +0100

    Big Merge
    Changelog:
    -Ny playerattack (Använder nu weaponObject)
    - Nytt Inventory
    - Store

--- Humanity's Last Chapter/Assets/MainMenu/Scripts/Combat/PlayerAttack.cs
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/Combat/PlayerAttack.cs
@@ -3,158 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using static Node;
-using Pathfinding;
 
-public class PlayerAttack : MonoBehaviour
-{
-    public float speed = 1.5f;
-    private Vector3 destination;
-    private bool walking;
+public class PlayerAttack : MonoBehaviour {
 
-    bool debugging = true;
-
-    private float attackTimer;
-    public float timeBetweenAttack;
-
-    public RectTransform attackPos;
-    public LayerMask whatIsEnemy;
-    public LayerMask Buildings;
-    RaycastHit2D hitInfo;
-
-    public float attackRange;
     public int damage;
-    public int loadout;
-    private string loadoutType = " ";
-
-    private Transform enemy;
-    private Vector3 enemyPos;
-    private float enemyDistance;
     public float aggroDistance;
+    public float attackRange;
+    public LayerMask humansAndBuildings;
+    public LayerMask whatIsEnemy;
 
+    private Vector3 enemyPos;
+    private RaycastHit2D hitInfo;
+
+    /* Notes:
+        Det finns två koder i detta script, toro kod (Vi gör alla vapen i kod) och Markus kod (Vi gör alla vapen som scriptable objects)
+        Båda kan inte användas samtidigt! För att byta kod krävs:
+
+        * Här:                      Avmarkera region koden du inte vill använda och markera den i den andra
+        * I Party script:           Markera/avmarkera Markus vapen skapande (rad 48 - 61)
+        * I BoiGun prefaben:        Markera/avmarkera Test Weapon (Markus Vapen)
+        * I Humanityboi prefaben:   Markera/avmarkera Weapon Script (Toros vapen skapande)
+         
+        Notes från markus: Anledningen till detta är för att visa hur mycket mer effektivt det är att ha Scriptable Objects!
+     */
+
+    #region Markus exclusive variabler
+    private WeaponAttack tWeapon;
+    #endregion
+
+    #region Toro exclusive variabler
+    public int loadout;
+    public float speed = 1.5f;
+    public float timeBetweenAttack;
+    public RectTransform attackPos;
     public GameObject projectile;
+
+    private bool walking;
+    private float enemyDistance;
+    private float attackTimer;
+    private string loadoutType = " ";
+    private Vector3 destination;
+    private Transform enemy;
     private GameObject projectile0;
+    #endregion
 
-    void Start()
-    {
-        //destination = transform.position;
-
-        foreach(GameObject item in gameObject.GetComponent<Stats>().items)
-        {
-            if(item.GetComponent<ItemScript>().IsActive())
-            {
-                switch(item.GetComponent<ItemScript>().ItemID)
-                    {
-                    case "Melee":
-                        loadout = 0;
-                        break;
-
-                    case "Pistol":
-                        if (debugging)
-                            Debug.Log("has pistol");
-                        loadout = 1;
-                        break;
-
-                    case "Shotgun":
-                        if (debugging)
-                            Debug.Log("has shotgun");
-                        loadout = 2;
-                        break;
-                }
-                break;
-            }
-        }
-        CheckLoadout();
-    }
-
-    void Update()
-    {
-        //For testing only
-        //TestMoveToDestination();
-        //TestCombat();
-
-        Debug.DrawLine(transform.position, GetNearestTarget().transform.position);
-
-        if (attackTimer > 0)
-            attackTimer -= Time.deltaTime;
-    }
-
-    public NodeStates LineOfSight()
-    {
-        hitInfo = Physics2D.Raycast(transform.position, GetNearestTarget().transform.position, aggroDistance, Buildings);
-        Debug.DrawLine(transform.position, hitInfo.point);
-        if (hitInfo)
-        {
+    //Gemensama funktioner behöver inte ändras för att markus eller toro kod ska fungera
+    #region Gemensama funktioner
+    public NodeStates LineOfSight() {
+        hitInfo = Physics2D.Raycast(transform.position, enemyPos, aggroDistance, humansAndBuildings);
+        if (hitInfo.collider != null) {
             return NodeStates.fail;
         }
-
         return NodeStates.success;
     }
 
-    public NodeStates IsRanged()
-    {
-        if (loadoutType != "Melee")
-            return NodeStates.success;
-        else
-            return NodeStates.fail;
-    }
-
-    public NodeStates InCombatRange()
-    {
-        GameObject go = GetNearestTarget();
-        if (go.Equals(gameObject))
-        {
-            return NodeStates.fail;
-        }
-
-        enemy = go.transform;
-        enemyPos = enemy.position;
-        enemyDistance = Vector3.Distance(enemyPos, transform.position);
-        if (attackTimer <= 0)
-        {
-            if (enemyDistance <= aggroDistance)
-            {
-                return NodeStates.success;
-            }
-        }
-        return NodeStates.fail;
-    }
-
-    public NodeStates RangeAttack()
-    {
-        //projectile0 = Instantiate(projectile, transform.position, Quaternion.identity);
-        //attackTimer = timeBetweenAttack;
-        TestCombat();
-        return NodeStates.success;
-    }
-
-    public NodeStates MeleeAttack()
-    {
-        //Debug.Log("melee");
+    public NodeStates MeleeAttack() {
         Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(transform.position, attackRange, whatIsEnemy);
 
-        for (int i = 0; i < enemiesToDamage.Length; i++)
-        {
+        for (int i = 0; i < enemiesToDamage.Length; i++) {
             enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
         }
         return NodeStates.success;
     }
 
-    public NodeStates MoveTowardsEnemy()
-    {
-        GameObject target = GetNearestTarget();
-        GetComponent<AIDestinationSetter>().SetPosTarget(target.transform.position);
+    public NodeStates MoveTowarsEnemy() {
+        GameObject target = tWeapon.closestEnemy;
+        //GetComponent<AIDestinationSetter>().SetPosTarget(target.transform.position);
         return NodeStates.success;
     }
-    public NodeStates WithinAggroRange()
-    {
-        if (Vector3.Distance(GetNearestTarget().transform.position,transform.position) < aggroDistance)
-        {
-            return NodeStates.success;
-        }
 
-        return NodeStates.fail;
+<<<<<<< HEAD
+    public NodeStates MeleeAttack()
+    {
+        //Debug.Log("melee");
+        Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(transform.position, attackRange, whatIsEnemy);
+=======
+    public NodeStates WithinAggroRange() {
+>>>>>>> MarkusTest
+
+        return NodeStates.success;
     }
 
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+    #endregion
+
+    #region Markus Kod
+    void Start() {
+        tWeapon = GetComponentInChildren<WeaponAttack>();
+    }
+
+<<<<<<< HEAD
     private void TestCombat()
     {
         //enemy = GetNearestTarget().transform;
@@ -191,57 +132,169 @@ public class PlayerAttack : MonoBehaviour
             {
                 projectile0 = Instantiate(projectile, transform.position, Quaternion.identity);
                 projectile0.GetComponent<Projectile>().CreateProjectile(1f);
+=======
+    public NodeStates IsRanged() {
+        if (tWeapon != null) {
+            if (tWeapon.IsRange()) {
+                return NodeStates.success;
+>>>>>>> MarkusTest
+            }
+        } else {
+            tWeapon = GetComponentInChildren<WeaponAttack>();
+        }
+        return NodeStates.fail;
+    }
+
+    public NodeStates InCombatRange() {
+        if (tWeapon != null) {
+            if (tWeapon.InCombat(aggroDistance)) {
+                return NodeStates.success;
             }
         }
-        attackTimer = timeBetweenAttack;
+        return NodeStates.fail;
     }
 
-    private void TestMoveToDestination()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            destination.z = transform.position.z;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+    public NodeStates RangeAttack() {
+        tWeapon = GetComponentInChildren<WeaponAttack>();
+        tWeapon.CreateBullet();
+        return NodeStates.success;
     }
 
-    GameObject GetNearestTarget()
-    {
-        if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
-        {
-            return gameObject;
-        }
 
-        return GameObject.FindGameObjectsWithTag("Enemy").Aggregate((o1, o2) => Vector3.Distance(o1.transform.position, this.transform.position) 
-        > Vector3.Distance(o2.transform.position, this.transform.position) ? o2 : o1);
-    }
+    #endregion
 
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
-    }
+    #region Toro Kod
+    //void Start() {
+    //    //destination = transform.position;
 
-    void CheckLoadout()
-    {
-        if (loadout == 0)
-        {
-            loadoutType = "Melee";
-            damage = 5;
-            timeBetweenAttack = 0.5f;
-        }
-        else if (loadout == 1)
-        {
-            loadoutType = "Pistol";
-            damage = 10;
-            timeBetweenAttack = 1;
-        }
-        else if (loadout == 2)
-        {
-            loadoutType = "Shotgun";
-            damage = 4;
-            timeBetweenAttack = 3;
-        }
-    }
+    //    foreach (GameObject item in gameObject.GetComponent<stats>().items) {
+    //        if (item.GetComponent<ItemScript>().IsActive()) {
+    //            switch (item.GetComponent<ItemScript>().ItemID) {
+    //                case "Melee":
+    //                    loadout = 0;
+    //                    break;
+
+    //                case "Pistol":
+    //                    loadout = 1;
+    //                    break;
+
+    //                case "Shotgun":
+    //                    loadout = 2;
+    //                    break;
+    //            }
+    //            break;
+    //        }
+    //    }
+    //    CheckLoadout();
+    //}
+
+    //public NodeStates IsRanged() {
+    //    if (loadoutType == "Pistol")
+    //        return NodeStates.success;
+    //    else
+    //        return NodeStates.fail;
+    //}
+
+    //public NodeStates InCombatRange() {
+    //    GameObject go = GetNearestTarget();
+    //    if (go.Equals(gameObject)) //Varför har ni så att GetNearestTarget returnar gameObject och inte null när det inte finns några fiender? och sen => if (go != null)
+    //    {
+    //        return NodeStates.fail;
+    //    }
+
+    //    enemy = go.transform;
+    //    enemyPos = enemy.position;
+    //    enemyDistance = Vector3.Distance(enemyPos, transform.position);
+    //    if (attackTimer <= 0) {
+    //        if (enemyDistance <= aggroDistance) {
+    //            return NodeStates.success;
+    //        }
+    //    }
+    //    return NodeStates.fail;
+    //}
+
+    //public NodeStates RangeAttack() {
+    //    TestCombat();
+    //    return NodeStates.success;
+    //}
+
+    //private void TestCombat() {
+    //    //enemy = GetNearestTarget().transform;
+    //    //enemyPos = enemy.position;
+    //    //enemyDistance = Vector3.Distance(enemyPos, transform.position);
+    //    //Debug.Log("Distance: " + enemyDistance);
+    //    //if (attackTimer <= 0)
+    //    //{
+    //    //    if (enemyDistance <= aggroDistance)
+    //    //    {
+    //    //        if (loadoutType == "Melee")
+    //    //        {
+    //    //            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(transform.position, attackRange, whatIsEnemy);
+
+    //    //            for (int i = 0; i < enemiesToDamage.Length; i++)
+    //    //            {
+    //    //                enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+    //    //            }
+    //    //        }
+
+    //    //    }
+    //    //}
+    //    if (loadoutType == "Pistol") {
+    //        //projectile0.GetComponent<Projectile>().spread = 0f;
+    //        projectile0 = Instantiate(projectile, transform.position, Quaternion.identity);
+    //        projectile0.GetComponent<Projectile>().CreateProjectile(0f);
+    //        //projectile0.GetComponent<Projectile>().character = gameObject;
+    //    }
+    //    if (loadoutType == "Shotgun") {
+
+    //        for (int i = 0; i < 4; i++) {
+    //            projectile0 = Instantiate(projectile, transform.position, Quaternion.identity);
+    //            projectile0.GetComponent<Projectile>().CreateProjectile(1f);
+    //        }
+    //    }
+    //    attackTimer = timeBetweenAttack;
+    //}
+
+    //void CheckLoadout() {
+    //    if (loadout == 0) {
+    //        loadoutType = "Melee";
+    //        damage = 5;
+    //        timeBetweenAttack = 0.5f;
+    //    } else if (loadout == 1) {
+    //        loadoutType = "Pistol";
+    //        damage = 10;
+    //        timeBetweenAttack = 1;
+    //    } else if (loadout == 2) {
+    //        loadoutType = "Shotgun";
+    //        damage = 4;
+    //        timeBetweenAttack = 3;
+    //    }
+    //}
+
+    //private void TestMoveToDestination() {
+    //    if (Input.GetMouseButtonDown(1)) {
+    //        destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        destination.z = transform.position.z;
+    //    }
+    //    transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+    //}
+
+    //void Update() {
+    //    //For testing only
+    //    //TestMoveToDestination();
+    //    //TestCombat();
+
+    //    if (attackTimer > 0)
+    //        attackTimer -= Time.deltaTime;
+    //}
+
+    //GameObject GetNearestTarget() {
+    //    if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0) {
+    //        return gameObject;
+    //    }
+
+    //    return GameObject.FindGameObjectsWithTag("Enemy").Aggregate((o1, o2) => Vector3.Distance(o1.transform.position, this.transform.position)
+    //    > Vector3.Distance(o2.transform.position, this.transform.position) ? o2 : o1);
+    //}
+    #endregion
 }
