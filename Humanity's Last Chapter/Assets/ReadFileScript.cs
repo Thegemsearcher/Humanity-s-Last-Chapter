916a062159b55e2349f3f9a88d1929edd86c7227
ChaacNado <35525138+ChaacNado@users.noreply.github.com>
2019-11-18 20:02:28 +0100

    read from file, script created and tested externally

---
+++ Humanity's Last Chapter/Assets/ReadFileScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class ReadFileScript
+{
+    public static string path;
+    public static int mapID; //Ex 01
+    static List<string> strings;
+    public static void ReadString()
+    {
+        //path = "MapFiles/pcgmap" + mapID + ".txt";
+        path = "MapFiles/pcgmap01.txt";
+        strings = new List<string>();
+        StreamReader sr = new StreamReader(path);
+        Debug.Log(sr.ReadToEnd());
+        while (!sr.EndOfStream)
+        {
+            strings.Add(sr.ReadLine());
+        }
+        sr.Close();
+    }
+
+    static void SplitString()
+    {
+        for (int i = 0; i < strings.Count; i++)
+        {
+            string[] coordinates = strings[i].Split(' ');
+            //for (int j = 0; j < coordinates.Length; j++)
+            //{
+            //    string[] xy = coordinates[j].Split();
+            //}
+        }
+    }
+}
