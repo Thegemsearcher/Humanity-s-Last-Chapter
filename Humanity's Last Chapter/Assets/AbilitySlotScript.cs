25399503c5c33c5dfb101d3c938a0e513e6caa00
David <davidskiold@yahoo.se>
2019-11-19 18:07:08 +0100

    påbörjat turret

---
+++ Humanity's Last Chapter/Assets/AbilitySlotScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilitySlotScript : MonoBehaviour
+{
+    public KeyCode BoundKey;
+    public GameObject AttachedAbility;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (AttachedAbility == null)
+        //    return;
+        if (Input.GetKeyDown(BoundKey))
+        {
+            Activate();
+        }
+    }
+
+    public void Activate()
+    {
+        Debug.Log(BoundKey + " Activated");
+        if (AttachedAbility == null)
+            return;
+        AttachedAbility.GetComponent<AbilityScript>().Activate();
+    }
+}
