d5106fda6cccc4569b29969e80402a4fee1df86d
David <davidskiold@yahoo.se>
2019-11-19 18:21:28 +0100

    Revert "merge 1.0"
    This reverts commit 40d0a4b7b157ba90a9373c6dec144fc52e1733d0, reversing
    changes made to f34234f2d330cddd166f31500b589cf22d0ee0b4.

--- Humanity's Last Chapter/Assets/ReadFileScript.cs
+++ Humanity's Last Chapter/Assets/ReadFileScript.cs
@@ -10,72 +10,43 @@ using System;
 public static class ReadFileScript
 {
     private static string mapPath;
-    private static string savePath;
+    private static string scenePath;
     public static int mapID;
     private static string[] words;
-    private static GameObject roadsPrefab;
-    private static GameObject wallsPrefab;
-    public static void ReadString(Sprite roadSprite, Sprite wallSprite)
+    public static void ReadString()
     {
         mapID = 1;
         mapPath = "MapFiles/pcgmap" + mapID + ".txt";
-        savePath = "Assets/MainMenu/Scenes/Map" + mapID;
+        scenePath = "Assets/MainMenu/Scenes/Map" + mapID + ".unity";
         StreamReader sr = new StreamReader(mapPath);
-        words = sr.ReadToEnd().Split(' ', '\n');
+        words = sr.ReadToEnd().Split(' ');
         sr.Close();
-        PreparePrefabs();
-        MapLoader(roadSprite, wallSprite);
+
+        MapLoader();
     }
 
-    private static void PreparePrefabs()
+    public static void MapLoader()
     {
-        roadsPrefab = new GameObject();
-        roadsPrefab.AddComponent<GameObjectList>();
-        roadsPrefab.GetComponent<GameObjectList>().SetType("Road");
-        wallsPrefab = new GameObject();
-        wallsPrefab.AddComponent<GameObjectList>();
-        wallsPrefab.GetComponent<GameObjectList>().SetType("Wall");
-    }
+        var newScene = SceneManager.CreateScene("Map" + mapID);
+        //SceneManager.LoadScene("Map" + mapID, LoadSceneMode.Additive);
 
-
-    public static void MapLoader(Sprite roadSprite, Sprite wallSprite)
-    {
-        for (int i = 0; i < words.Length; i++)
-        {
-            if (words[i] == "b")
-            {
-                NewBuilding(Convert.ToInt32(words[i + 1]));
-            }
-            else if (words[i] == "w")
-            {
-                NewWall(Convert.ToSingle(words[i + 1].Replace(',', '.')), Convert.ToSingle(words[i + 2].Replace(',', '.')), Convert.ToSingle(words[i + 3].Replace(',', '.')), Convert.ToSingle(words[i + 4].Replace(',', '.')));
-            }
-            else if (words[i] == "r")
-            {
-                NewRoad(Convert.ToSingle(words[i + 1].Replace(',', '.')), Convert.ToSingle(words[i + 2].Replace(',', '.')));
-            }
-        }
-
-        foreach (GameObject wall in wallsPrefab.GetComponent<GameObjectList>().GetObjects())
-        {
-            wall.AddComponent<SpriteRenderer>();
-            wall.GetComponent<SpriteRenderer>().sprite = wallSprite;
-            wall.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Tiled;
-            wall.GetComponent<SpriteRenderer>().size = new Vector2(wall.GetComponent<WallScript>().GetValue(2), wall.GetComponent<WallScript>().GetValue(3));
-            wall.AddComponent<BoxCollider2D>();
-            wall.GetComponent<BoxCollider2D>().size = new Vector2(wall.GetComponent<WallScript>().GetValue(2), wall.GetComponent<WallScript>().GetValue(3));
-            wall.transform.position = new Vector3(wall.GetComponent<WallScript>().GetValue(0), wall.GetComponent<WallScript>().GetValue(1), 0);
-        }
-        foreach (GameObject road in roadsPrefab.GetComponent<GameObjectList>().GetObjects())
-        {
-            road.AddComponent<SpriteRenderer>();
-            road.GetComponent<SpriteRenderer>().sprite = roadSprite;
-            road.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Tiled;
-            road.GetComponent<SpriteRenderer>().size = new Vector2(1, 1);
-            road.transform.position = new Vector3(road.GetComponent<RoadScript>().GetValue(0), road.GetComponent<RoadScript>().GetValue(1), 0);
-        }
-        PrefabUtility.SaveAsPrefabAsset(roadsPrefab, savePath + ".roads.prefab");
-        PrefabUtility.SaveAsPrefabAsset(wallsPrefab, savePath + ".walls.prefab");
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //for (int i = 0; i < words.Length; i++)
+        //{
+        //    if (words[i] == "b")
+        //    {
+        //        NewBuilding(Convert.ToInt32(words[i + 1]));
+        //    }
+        //    else if (words[i] == "w")
+        //    {
+        //        NewWall(Convert.ToInt32(words[i + 1]), Convert.ToInt32(words[i + 2]), Convert.ToInt32(words[i + 3]), Convert.ToInt32(words[i + 4]));
+        //    }
+        //    else if (words[i] == "r")
+        //    {
+        //        NewRoad(Convert.ToInt32(words[i + 1]), Convert.ToInt32(words[i + 2]));
+        //    }
+        //}
+        EditorSceneManager.SaveScene(newScene, scenePath);
     }
 
     private static void NewBuilding(int ID)
@@ -85,12 +56,12 @@ public static class ReadFileScript
 
     private static void NewWall(float posX, float posY, float width, float height)
     {
-        wallsPrefab.GetComponent<GameObjectList>().Add(posX, posY, width, height);
+
     }
 
     private static void NewRoad(float posX, float posY)
     {
-        roadsPrefab.GetComponent<GameObjectList>().Add(posX, posY);
+
     }
 
     private static void EditAsset(string str)
