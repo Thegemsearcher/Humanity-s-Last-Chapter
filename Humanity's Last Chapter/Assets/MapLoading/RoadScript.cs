d2da4ed9f65f860b3aa48c0f2e18dcf3367b15dd
David <davidskiold@yahoo.se>
2019-11-19 18:32:31 +0100

    Revert "Revert "merge 1.0""
    This reverts commit d5106fda6cccc4569b29969e80402a4fee1df86d.

---
+++ Humanity's Last Chapter/Assets/MapLoading/RoadScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadScript : MonoBehaviour
+{
+    List<float> values = new List<float>();
+    public void AddValue(float x)
+    {
+        values.Add(x);
+    }
+    public float GetValue(int x)
+    {
+        return values[x];
+    }
+}
---
+++ Humanity's Last Chapter/Assets/MapPrefabFinisher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPrefabFinisher : MonoBehaviour
+{
+    public Sprite roadSprite;
+    public Sprite wallSprite;
+    void Start()
+    {
+        foreach(GameObject road in GameObject.FindGameObjectsWithTag("road"))
+        {
+            road.AddComponent<SpriteRenderer>();
+            road.GetComponent<SpriteRenderer>().sprite = roadSprite;
+            road.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Tiled;
+            road.GetComponent<SpriteRenderer>().size = new Vector2(road.GetComponent<RoadScript>().GetValue(2), road.GetComponent<RoadScript>().GetValue(3));
+            road.AddComponent<BoxCollider2D>();
+            road.GetComponent<BoxCollider2D>().size = new Vector2(road.GetComponent<RoadScript>().GetValue(2), road.GetComponent<RoadScript>().GetValue(3));
+            road.transform.position = new Vector3(road.GetComponent<RoadScript>().GetValue(0), road.GetComponent<RoadScript>().GetValue(1), 0);
+        }
+        foreach (GameObject wall in GameObject.FindGameObjectsWithTag("wall"))
+        {
+            wall.AddComponent<SpriteRenderer>();
+            wall.GetComponent<SpriteRenderer>().sprite = wallSprite;
+            wall.GetComponent<SpriteRenderer>().drawMode = SpriteDrawMode.Simple;
+            wall.GetComponent<SpriteRenderer>().size = new Vector2();
+            wall.transform.position = new Vector3(wall.GetComponent<RoadScript>().GetValue(0), wall.GetComponent<RoadScript>().GetValue(1), 0);
+        }
+    }
+}
