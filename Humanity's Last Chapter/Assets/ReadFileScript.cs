15e2d715b83408b36e772d3f7f1b839676e39067
ChaacNado <35525138+ChaacNado@users.noreply.github.com>
2019-11-19 15:06:54 +0100

    read from file 2.0
    wip

--- Humanity's Last Chapter/Assets/ReadFileScript.cs
+++ Humanity's Last Chapter/Assets/ReadFileScript.cs
@@ -1,37 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using System.IO;
+using System;
 
 public static class ReadFileScript
 {
-    public static string path;
-    public static int mapID; //Ex 01
-    static List<string> strings;
+    private static string mapPath;
+    private static string scenePath;
+    public static int mapID;
+    private static string[] words;
     public static void ReadString()
     {
-        //path = "MapFiles/pcgmap" + mapID + ".txt";
-        path = "MapFiles/pcgmap01.txt";
-        strings = new List<string>();
-        StreamReader sr = new StreamReader(path);
-        Debug.Log(sr.ReadToEnd());
-        while (!sr.EndOfStream)
-        {
-            strings.Add(sr.ReadLine());
-        }
+        mapID = 1;
+        mapPath = "MapFiles/pcgmap" + mapID + ".txt";
+        scenePath = "Assets/MainMenu/Scenes/Map" + mapID + ".unity";
+        StreamReader sr = new StreamReader(mapPath);
+        words = sr.ReadToEnd().Split(' ');
         sr.Close();
+
+        MapLoader();
     }
 
-    static void SplitString()
+    public static void MapLoader()
     {
-        for (int i = 0; i < strings.Count; i++)
-        {
-            string[] coordinates = strings[i].Split(' ');
-            //for (int j = 0; j < coordinates.Length; j++)
-            //{
-            //    string[] xy = coordinates[j].Split();
-            //}
-        }
+        var newScene = SceneManager.CreateScene("Map" + mapID);
+        //SceneManager.LoadScene("Map" + mapID, LoadSceneMode.Additive);
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //for (int i = 0; i < words.Length; i++)
+        //{
+        //    if (words[i] == "b")
+        //    {
+        //        NewBuilding(Convert.ToInt32(words[i + 1]));
+        //    }
+        //    else if (words[i] == "w")
+        //    {
+        //        NewWall(Convert.ToInt32(words[i + 1]), Convert.ToInt32(words[i + 2]), Convert.ToInt32(words[i + 3]), Convert.ToInt32(words[i + 4]));
+        //    }
+        //    else if (words[i] == "r")
+        //    {
+        //        NewRoad(Convert.ToInt32(words[i + 1]), Convert.ToInt32(words[i + 2]));
+        //    }
+        //}
+        EditorSceneManager.SaveScene(newScene, scenePath);
+    }
+
+    private static void NewBuilding(int ID)
+    {
+
+    }
+
+    private static void NewWall(float posX, float posY, float width, float height)
+    {
+
+    }
+
+    private static void NewRoad(float posX, float posY)
+    {
+
+    }
+
+    private static void EditAsset(string str)
+    {
+
     }
 }
