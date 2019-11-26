bac5ee7cf63812cc1cebc6ad652e67c339db84fe
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-11-26 13:06:27 +0100

    Implementerade CurvedText
    Ge ett Object componenten CurvedText och du kan skriva saker så att det går som en cirkel

---
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/UI/TextEditor/CurvedTextEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CurvedText))]
+public class CurvedTextEditor : Editor { //Markus - Gör så att man kan modifiera den curvade texten i Scene
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+    }
+}
