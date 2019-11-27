e36e1e4d107d6e40504fecd1873a1e75f6ee0aa3
patrik-nilsson <patriknilson@hotmail.com>
2019-11-27 17:01:44 +0100

    Glow added to buildings!
    WOW - Owen Wilson

---
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/BuildingScripts/GlowScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowScript : MonoBehaviour
+{
+    GameObject Child;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.CompareTag("Glow"))
+                Child = child.gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<BoxCollider2D>().OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+        {
+            Child.gameObject.SetActive(true);
+        }
+        else if (Child.activeSelf)
+        {
+            Child.gameObject.SetActive(false);
+        }
+    }
+}
