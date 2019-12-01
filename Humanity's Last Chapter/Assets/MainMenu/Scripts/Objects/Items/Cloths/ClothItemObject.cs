facd84866e82357b92290f6b86626e471ea770ad
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-12-01 20:27:58 +0100

    PåBörjad Del att skapa de första karaktärena
    Changelog:
    - La till ClothObject så att kläder kan ge stats förändringar
    - Skapade en texture för att skapa de första karaktärena

---
+++ Humanity's Last Chapter/Assets/MainMenu/Scripts/Objects/Items/Cloths/ClothItemObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ClothScript;
+
+[System.Serializable]
+public class ClothItemObject : ScriptableObject {
+
+    public Sprite icon;
+    public Sprite sprite;
+    public string itemName = "Red Jacket";
+    public string description = "A warm red jacket";
+    public int cost = 10;
+    public ClothType clothType;
+
+    //Boost the Item gives while wearing it
+    public int maxHp = 0;
+    public int str = 0;
+    public int def = 0;
+    public int Int = 0;
+    public int dex = 0;
+    public int cha = 0;
+    public int ldr = 0;
+    public int nrg = 0;
+    public int snt = 0; 
+}
