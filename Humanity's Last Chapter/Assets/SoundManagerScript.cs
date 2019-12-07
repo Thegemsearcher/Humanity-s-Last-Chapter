e5543c612ae88fee75b4cacc5cf6b7ae6801b772
ChaacNado <35525138+ChaacNado@users.noreply.github.com>
2019-12-07 22:48:52 +0100

    Improved sfx and soundmanager script

--- Humanity's Last Chapter/Assets/SoundManagerScript.cs
+++ Humanity's Last Chapter/Assets/SoundManagerScript.cs
@@ -4,64 +4,94 @@ using UnityEngine;
 
 public class SoundManagerScript : MonoBehaviour
 {
-    public static AudioClip pistolSound, shotgunSound, ARSound, rifleSound, deathSound, pickupSound, clickSound;
+    //public static AudioClip pistolSound, shotgunSound, ARSound, rifleSound, deathSound, pickupSound, clickSound;
     private static AudioSource pistolSrc, shotgunSrc, ARSrc, rifleSrc, deathSrc, pickupSrc, clickSrc;
 
     // Start is called before the first frame update
     void Start()
     {
-        pistolSound = Resources.Load<AudioClip>("pistol");
-        shotgunSound = Resources.Load<AudioClip>("shotgun");
-        ARSound = Resources.Load<AudioClip>("AR");
-        rifleSound = Resources.Load<AudioClip>("rifle");
-        deathSound = Resources.Load<AudioClip>("fart");
-        pickupSound = Resources.Load<AudioClip>("pickup");
-        clickSound = Resources.Load<AudioClip>("click");
+        //pistolSound = Resources.Load<AudioClip>("pistol");
+        //shotgunSound = Resources.Load<AudioClip>("shotgun");
+        //ARSound = Resources.Load<AudioClip>("AR");
+        //rifleSound = Resources.Load<AudioClip>("rifle");
+        //deathSound = Resources.Load<AudioClip>("fart");
+        //pickupSound = Resources.Load<AudioClip>("pickup");
+        //clickSound = Resources.Load<AudioClip>("click");
 
-        pistolSrc = GetComponent<AudioSource>();
-        shotgunSrc = GetComponent<AudioSource>();
-        ARSrc = GetComponent<AudioSource>();
-        rifleSrc = GetComponent<AudioSource>();
-        deathSrc = GetComponent<AudioSource>();
-        pickupSrc = GetComponent<AudioSource>();
-        clickSrc = GetComponent<AudioSource>();
+        AudioSource[] audios = GetComponents<AudioSource>();
+        pistolSrc = audios[0];
+        shotgunSrc = audios[1];
+        ARSrc = audios[2];
+        rifleSrc = audios[3];
+        deathSrc = audios[4];
+        pickupSrc = audios[5];
+        clickSrc = audios[6];
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            pistolSrc.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            shotgunSrc.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ARSrc.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            rifleSrc.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            deathSrc.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            pickupSrc.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            clickSrc.Play();
+        }
     }
 
     public static void PlaySound(string clip)
     {
-        if (clip == "pistol")
-        {
-            pistolSrc.PlayOneShot(pistolSound);
-        }
-        if (clip == "shotgun")
-        {
-            shotgunSrc.PlayOneShot(shotgunSound);
-        }
-        if (clip == "AR")
-        {
-            ARSrc.PlayOneShot(ARSound);
-        }
-        if (clip == "rifle")
-        {
-            rifleSrc.PlayOneShot(rifleSound);
-        }
-        if (clip == "death")
-        {
-            deathSrc.PlayOneShot(deathSound);
-        }
-        if (clip == "pickup")
-        {
-            pickupSrc.PlayOneShot(pickupSound);
-        }
-        if (clip == "click")
-        {
-            clickSrc.PlayOneShot(clickSound);
-        }
+        //For sound testing
+
+        //if (clip == "pistol")
+        //{
+        //    pistolSrc.Play();
+        //}
+        //if (clip == "shotgun")
+        //{
+        //    shotgunSrc.Play();
+        //}
+        //if (clip == "AR")
+        //{
+        //    ARSrc.Play();
+        //}
+        //if (clip == "rifle")
+        //{
+        //    rifleSrc.Play();
+        //}
+        //if (clip == "death")
+        //{
+        //    deathSrc.Play();
+        //}
+        //if (clip == "pickup")
+        //{
+        //    pickupSrc.Play();
+        //}
+        //if (clip == "click")
+        //{
+        //    clickSrc.Play();
+        //}
     }
 }
