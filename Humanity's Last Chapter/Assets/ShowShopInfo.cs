c2f51512634cc0107789fdc728c9a4d82081fbbd
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2020-01-14 20:02:18 +0100

    Upgrade Info of shop

---
+++ Humanity's Last Chapter/Assets/ShowShopInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ShowShopInfo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public GameObject Shop;
+    public Text txtName, txtDesc, txtCost;
+
+    private UpgradeShop shopScript;
+
+    private void Start() {
+        shopScript = Shop.GetComponent<UpgradeShop>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        txtName.text = "Upgrade the Shop!";
+        txtDesc.text = "Upgrade the shop to level " + (shopScript.shopLevel + 1) + "! A better shop will provide better items you can buy!";
+        txtCost.text = "Cost: " + shopScript.upgradeCost * shopScript.shopLevel;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        txtName.text = "";
+        txtDesc.text = "";
+        txtCost.text = "";
+    }
+
+}
