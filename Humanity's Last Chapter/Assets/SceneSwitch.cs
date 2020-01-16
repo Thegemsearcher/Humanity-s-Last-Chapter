33033f72ee3638b92dc4eb760dca9b000e941700
Thegemsearcher <jacklewitan@hotmail.com>
2020-01-16 23:04:59 +0100

    Fixed tutorial
    now it uses Enter

--- Humanity's Last Chapter/Assets/SceneSwitch.cs
+++ Humanity's Last Chapter/Assets/SceneSwitch.cs
@@ -14,7 +14,7 @@ public class SceneSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             SceneManager.LoadScene("SaveScreen");
         }
