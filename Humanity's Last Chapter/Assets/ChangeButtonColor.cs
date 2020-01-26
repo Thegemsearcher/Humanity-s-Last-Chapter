81de8618f9635f055d1152edb7bb8335e79db2e3
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2020-01-26 15:12:16 +0100

    Merge

---
+++ Humanity's Last Chapter/Assets/ChangeButtonColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ChangeButtonColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Image color;
+    void Start()
+    {
+        color = GetComponent<Image>();
+        color.GetComponent<Image>().color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        
+    }
+
+    public void ChangeColor()
+    {
+        color.GetComponent<Image>().color = Color.green;
+    }
+}
---
+++ Humanity's Last Chapter/Assets/ReturnToHubText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ReturnToHubText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text test;
+    void Start()
+    {
+        test = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+    public void ChangeText()
+    {
+        test.text = "Success!! Return to hub to reap your rewards";
+    }
+}
